11260ea1f3e873d2ca600cde988d49b17bce4753
toluooshy <56281958+toluooshy@users.noreply.github.com>
2025-12-31 19:46:06 -0500

    Fixing breaking changes.

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.use((req, res) => {
 
 /* -------------------- SERVER -------------------- */
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
